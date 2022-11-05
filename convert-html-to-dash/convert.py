dc3948ccfe421e8489e766ccb3a661c8e5b81353
majjikishore <majjikishore@localhost.localdomain>
2022-11-05 19:03:49 +0530

    replaced the depricated import statement to latest one

--- convert-html-to-dash/convert.py
+++ convert-html-to-dash/convert.py
@@ -2,9 +2,8 @@ import os
 import argparse
 import signal
 import sys
-import dash_html_components as html
+from dash import html, dcc
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
 
 from black import format_str, FileMode
 from lxml import etree
